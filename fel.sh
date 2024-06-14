b093cb23255d7c12df369a8b6b11eb6568232c43
Ali Eriş <v9zz@hotmail.com>
2024-06-14 21:35:41 +0300

    Update fel.sh

--- fel.sh
+++ fel.sh
@@ -1,1 +1,1 @@
-adb pull ./sdcard/Android/media/com.whatsapp/WhatsApp/Media/"WhatsApp Images"/IMG-20240614-WA008.jpg
+adb pull /sdcard/Android/media/com.whatsapp/WhatsApp/Media/"WhatsApp Images"/IMG-20240614-WA0008.jpg
